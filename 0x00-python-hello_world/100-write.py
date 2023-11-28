30fe5c7db708e35899906deb07ed691149a6bb77
Abdoul azize <lepronet85@gmail.com>
2023-11-27 17:09:42 -0800

    Add 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19/n"
-sys.stderr.write(message)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19/n")
 sys.exit(1)
