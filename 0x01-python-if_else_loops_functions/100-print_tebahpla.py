e3cca37abd45a03babf83b59fb11ae4f16ad6503
Abdoul azize <lepronet85@gmail.com>
2023-11-28 17:07:50 -0800

    Add 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i)), end="")
-    i -= 1
-    print("{}".format(chr(i).upper()), end="")
+    print("{}".format(chr(i).lower() if i % 2 == 0 else chr(i).upper()), end="")
