36af65b628f7ef1a5960a031f26c92ceb05c7545
Abdoul azize <lepronet85@gmail.com>
2023-11-30 10:30:44 -0800

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
 
-    # Importing the function add from add_0.py
-    import importlib
-    add_module = importlib.import_module("add_0")
-    add = add_module.add
-
-    result = add(a, b)
     print(f"{a} + {b} = {result}")
