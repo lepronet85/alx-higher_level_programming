c260bf377cd1c260785ce701df5baff2bdd587aa
Abdoul azize <lepronet85@gmail.com>
2023-12-06 09:10:18 -0800

    Add 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+
+    for row in matrix:
+        squared_row = []
+        for element in row:
+            squared_row.append(element ** 2)
+        squared_matrix.append(squared_row)
+    return squared_matrix
