abd2f6ed102b129fd2b1718413a75ded03a78e4e
Abdoul azize <lepronet85@gmail.com>
2023-12-06 09:16:49 -0800

    Add 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return my_list
+
+    new_list = []
+
+    for elem in my_list:
+        if elem == search:
+            new_list.appen(replace)
+        else:
+            new_list.append(elem)
+    return new_list
