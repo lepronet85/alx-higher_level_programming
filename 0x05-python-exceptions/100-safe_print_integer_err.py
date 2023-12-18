15b4ac4cd083db22ac2469247b1a60a950abfa82
Abdoul azize <lepronet85@gmail.com>
2023-12-18 15:58:18 -0800

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_integer_err(value):)
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as ve:
