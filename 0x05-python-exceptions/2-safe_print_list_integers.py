23e7d5076f1d9cfb4a68407b7e65897da4fcd7e9
Abdoul azize <lepronet85@gmail.com>
2023-12-18 12:15:19 -0800

    Add 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed_integers = 0
+    try:
+        for item in my_list[:x]:
+            if isinstance(item, int):
+                print("{:d}".format(item), end=' ')
+                printed_integers += 1
+        print()
+    except TypeError:
+        pass
+    return printed_integers
