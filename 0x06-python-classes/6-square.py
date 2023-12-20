88d1f286015bb6d7224d9978a6020dee9a034756
Abdoul azize <lepronet85@gmail.com>
2023-12-19 16:14:44 -0800

    Add 6-square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
