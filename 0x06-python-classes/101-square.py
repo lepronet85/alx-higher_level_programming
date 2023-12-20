8c5814a8643883b761707ebab3fabf64a870bb28
Abdoul azize <lepronet85@gmail.com>
2023-12-19 16:34:55 -0800

    Add 101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """class Square that defines a square """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        string = ""
+        if self.__size == 0:
+            return string
+        else:
+            for _ in range(self.__position[1]):
+                string += "\n"
+            for _ in range(self.__size):
+                string += " " * self.__position[0] + "#" * self.__size + "\n"
+            return string.rstrip("\n")
+
