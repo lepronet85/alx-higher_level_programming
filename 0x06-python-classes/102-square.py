948eaf45fbee9c0dea5c8d6dad6aa3ba46317644
Abdoul azize <lepronet85@gmail.com>
2023-12-19 16:41:04 -0800

    Add 102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
