0982877d4e03cc76a68984b13f1c61ef816e8943
lepronet85 <lepronet85@gmail.com>
2024-01-04 15:19:59 -0800

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -63,8 +63,9 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
-
+            solutions = recursive_solve(
+                tmp_board, row + 1, queens + 1, solutions
+            )
     return solutions
 
 
@@ -83,4 +84,3 @@ if __name__ == "__main__":
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
         print(sol)
-
