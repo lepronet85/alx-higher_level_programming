6d711e2f2cf4e657085c15ea8d995659f35cd6ee
lepronet85 <lepronet85@gmail.com>
2024-01-08 15:19:13 -0800

    Add 5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+
+Contains an empty class representing BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing BaseGeometry.
+    """
+    pass
