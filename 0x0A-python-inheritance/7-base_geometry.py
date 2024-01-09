5a2f05b363ee0e47e7917e0202e87e4a1de41c40
lepronet85 <lepronet85@gmail.com>
2024-01-08 16:32:38 -0800

    Last update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,28 +1,58 @@
-#!/usr/bin/python3
-"""
-Module: 7-base_geometry.py
-Defines a base geometry class BaseGeometry.
-"""
+==============================
+Use 7-base_geometry module
+==============================
 
+>>> BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """Reprsent base geometry."""
+::
+    >>> base1 = BaseGeometry()
+    >>> base1.area()
+    Traceback (most recent call last):
+    Exception: area() is not implemented
 
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
+::
 
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
+    >>> base2 = BaseGeometry()
+    >>> base2.integer_validator("name", -5)
+    Traceback (most recent call last):
+    ValueError: name must be greater than 0
 
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+::
+
+    >>> base3 = BaseGeometry()
+    >>> base3.integer_validator("name", 0)
+    Traceback (most recent call last):
+    ValueError: name must be greater than 0
+
+::
+
+    >>> base4 = BaseGeometry()
+    >>> base4.integer_validator("name", 7)
+
+::
+
+    >>> base5 = BaseGeometry()
+    >>> base5.integer_validator("name")
+    Traceback (most recent call last):
+    TypeError: integer_validator() missing 1 required positional argument: 'value'
+
+::
+
+    >>> base6 = BaseGeometry()
+    >>> base6.integer_validator("name", 8, 4)
+    Traceback (most recent call last):
+    TypeError: integer_validator() takes 3 positional arguments but 4 were given
+
+::
+
+    >>> base7 = BaseGeometry()
+    >>> base7.integer_validator("name", "2")
+    Traceback (most recent call last):
+    TypeError: name must be an integer
+
+::
+
+    >>> base8 = BaseGeometry()
+    >>> base8.integer_validator("age", {5, 12})
+    Traceback (most recent call last):
+    TypeError: age must be an integer
