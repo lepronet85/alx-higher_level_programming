44f566b2f8590a7e85f964a55a80ba45971de60d
lepronet85 <lepronet85@gmail.com>
2024-01-17 00:57:16 -0800

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,25 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Creates an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes with provided values"""
+        if args:
+            attr_names = ["id", "width", "height", "x", "y"]
+            for i, value in enumerate(args):
+                setattr(self, attr_names[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
