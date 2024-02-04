1019a789690012e6dc7c2d84e969397a906a42de
lepronet85 <lepronet85@gmail.com>
2024-02-04 08:43:33 -0800

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,7 @@
 """Defines a matrix division function."""
 
 
-def divide_matrix(matrix, divisor):
+def matrix_divided(matrix, div):
     """Divide all elements of a matrix by a given divisor.
 
     Args:
@@ -21,19 +21,16 @@ def divide_matrix(matrix, divisor):
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
                     for ele in [num for row in matrix for num in row])):
-        raise TypeError("Input matrix must be a list of lists of "
+        raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(divisor, int) and not isinstance(divisor, float):
-        raise TypeError("Divisor must be a number")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
 
-    if divisor == 0:
-        raise ZeroDivisionError("Division by zero is not allowed")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-    return [
-        [round(element / divisor, 2) for element in row]
-        for row in matrix
-    ]
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
