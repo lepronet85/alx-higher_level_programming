3805d3735aba6ec2247f8dd53344eec203b322ba
lepronet85 <lepronet85@gmail.com>
2024-02-04 09:14:18 -0800

    Update tests

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module containing a function to print first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the full name.
+
+    Argments:
+        @first_name: The first name.
+        @last_name: The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
