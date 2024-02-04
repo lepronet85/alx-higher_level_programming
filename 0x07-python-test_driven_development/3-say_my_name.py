62ae679f700883f220c497f07814fade4f103763
lepronet85 <lepronet85@gmail.com>
2024-02-04 09:20:53 -0800

    Fix pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,6 +9,7 @@ def say_my_name(first_name, last_name=""):
             @second_name: last_name to be printed
     """
 
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
