7ed5acbb4fda3561a2fb29ce7ce93aaf7770b7cf
lepronet85 <lepronet85@gmail.com>
2024-02-15 00:55:25 +0100

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,9 +2,9 @@
 SELECT name
 FROM tv_genres
 WHERE id NOT IN (
-    SELECT genre_id
-    FROM tv_shows
-    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-    WHERE title = 'Dexter'
+  SELECT genre_id
+  FROM tv_shows
+  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+  WHERE title = 'Dexter'
 )
 ORDER BY name ASC;
