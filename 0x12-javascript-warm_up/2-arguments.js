eb6fc99d1ae1f203bb004799cbdde5b4a90d5a74
lepronet85 <lepronet85@gmail.com>
2024-03-12 09:56:30 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const [, , ...args] = process.argv;
-
-if (args.length === 0)
-  console.log('No argument');
-else if (args.length === 1)
-  console.log('Argument found');
-else
-  console.log('Arguments found');
+const { argv } = require('process');
+if (argv.length === 2) {
+    console.log('No argument');
+} else if (argv.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
