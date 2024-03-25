d4e40d429731ed3207697157a39ee107ae528c1f
lepronet85 <lepronet85@gmail.com>
2024-03-25 10:19:36 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Defines a function for reading text files."""
+
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end='')
+    """Prints the contents of a UTF-8 encoded text file to standard output."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
