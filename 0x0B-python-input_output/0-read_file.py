5fe5336f8c5a4ffcd9f13206f37ae84732788ea6
lepronet85 <lepronet85@gmail.com>
2024-03-25 10:20:26 +0100

    Add pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 
 """Defines a function for reading text files."""
 
+
 def read_file(filename=""):
     """Prints the contents of a UTF-8 encoded text file to standard output."""
     with open(filename, encoding="utf-8") as f:
