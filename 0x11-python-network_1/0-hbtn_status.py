ab486f9c99eba4b9ca58e443f54451f6cca2ba70
lepronet85 <lepronet85@gmail.com>
2024-03-28 23:51:03 +0000

    Make file executable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-"""Fetches the status from https://intranet.hbtn.io/status"""
-
+ https://intranet.hbtn.io/status"""
 from urllib import request
 
 if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
-        print("Response body:")
+        print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
         print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
