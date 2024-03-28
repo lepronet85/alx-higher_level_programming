d59da7e47462bb685f4a5f3511a8e4864c658e34
lepronet85 <lepronet85@gmail.com>
2024-03-27 17:01:37 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Fetches the content of the URL provided as argument and displays
-# the size of the response body in bytes
+# This script takes in a URL, sends a request to that URL,and displays the size of the body of the response
 curl -s "$1" | wc -c
