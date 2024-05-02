8bc11b0adeaa4ffd4aab9e3c9b0e57e6a5855e4b
Abdoul aziz <lepronet85@gmail.coml>
2024-05-02 17:14:13 +0100

    Add files

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  function fetchTranslation() {
+    const languageCode = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data){
+      $('#hello').text(data.hello);
+    });
+  }
+
+  $('#btn_translate').click(fetchTranslation);
+
+  $('#language_code').keypress(function(e) {
+    if (e.which == 13) {
+      fetchTranslation();
+    }
+  });
+});
+
